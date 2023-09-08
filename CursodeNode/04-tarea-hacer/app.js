3e87405cf74ce8b8c8fbe88e45b0317d39efb2d6
dev-ernesto <deadjosesk@gmail.com>
2023-09-08 15:41:28 -0600

    seccion

---
+++ CursodeNode/04-tarea-hacer/app.js
@@ -0,0 +1,15 @@
+require('colors');
+const { mostrarMenu, pausa } = require('./helper/mensajes');
+
+const main = async() => {
+  console.clear;
+  let opt = ''
+  do {
+    opt =await mostrarMenu();
+    console.log({opt});
+    if (opt !== '0') await pausa();
+    
+  } while (opt !== '0');
+}
+
+main();
